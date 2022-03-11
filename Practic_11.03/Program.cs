3d3c23553dc231fa868ba03fe771f8c08f1f565e
avbokov <bokov.itstep@gmail.com>
2022-03-11 18:40:16 +0700

    1.1

---
+++ Practic_11.03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if false
+namespace Practic_11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+} 
+#endif
+
+namespace Practic_11._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* можно перегрузить
+             -
+            !
+            ~
+            ++ --
+            true false
+            + - * / %
+            & | ^ << >>
+            == != < > <= >=
+            && ||
+            []
+            ()
+
+                нельзя перегрузить
+            += -= *= /= %= &= |= ^= <<= >>=
+            =
+            .
+            ?:
+            new
+            as is typeof
+            -> sizeof * &
+             */
+
+            // основной синтаксис
+            //public static тип_возврата operator символ_операции (параметры) {}
+
+        }
+    }
+}
