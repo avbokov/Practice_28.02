c2a00d892ec452c296b874800232dc94d0bc8c4c
avbokov <bokov.itstep@gmail.com>
2022-03-20 20:54:02 +0700

    HW

--- HomeWork_module_02/Program.cs
+++ HomeWork_module_02/Program.cs
@@ -25,7 +25,8 @@ namespace HomeWork_modul_02
                 arr1[i] = Int32.Parse(Console.ReadLine());
                 if (i == 0) { min1 = max1 = arr1[i]; }
                 sum1 += arr1[i];
-                if (i % 2 == 0) sum_chet += arr1[i];    // сумма по чётным индексам (я так понял задание). Если нужно суммировать чётные значения, тут легко переделать.
+                //if (i % 2 == 0) sum_chet += arr1[i];      // сумма по чётным индексам (я так понял задание).
+                if (arr1[i] % 2 == 0) sum_chet += arr1[i];  // Если нужно суммировать чётные значения, тут легко переделать.
                 if (arr1[i] < min1) min1 = arr1[i];
                 if (arr1[i] > max1) max1 = arr1[i];
                 mult1 *= arr1[i];
@@ -60,8 +61,12 @@ namespace HomeWork_modul_02
             Console.WriteLine($"Сумма элементов в нечётных столбцах массива В: {sum_nech_cols}");
             Console.WriteLine($"Общая сумма элементов массивов: {sum1 + sum2}");
             Console.WriteLine($"Общее произведение элементов массивов: {mult1 * mult2}");
-            Console.WriteLine($"Общий максимальный элемент: {(max1 > max2 ? max1 : max2)}");
-            Console.WriteLine($"Общий минимальный элемент: {(min1 < min2 ? min1 : min2)}"); 
+            if (max1 == max2) Console.WriteLine($"Общий максимальный элемент: {max1}");
+            else Console.WriteLine($"Общего максимального элемента нет");
+            if (min1 == min2) Console.WriteLine($"Общий максимальный элемент: {min1}");
+            else Console.WriteLine($"Общего минимального элемента нет");
+            //Console.WriteLine($"Общий максимальный элемент: {(max1 > max2 ? max1 : max2)}");
+            //Console.WriteLine($"Общий минимальный элемент: {(min1 < min2 ? min1 : min2)}"); 
 #endif
 
             // Задание 2
@@ -189,6 +194,193 @@ namespace HomeWork_modul_02
                 Console.Write(i); 
 #endif
             // Задание 4
+
+#if false
+            const int ROWS1 = 3;
+            const int COLS1 = 3;
+
+            int[,] arr1 = new int[ROWS1, COLS1];
+            int[,] arr2 = new int[ROWS1, COLS1];
+            int[,] arr_composition = new int[ROWS1, COLS1];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    arr1[i,j] = rnd.Next(10);
+                    arr2[i,j] = rnd.Next(10);                   
+                }
+            }
+
+            Console.WriteLine("Матрица 1:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr1[i,j]} ");                  
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Матрица 2:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr2[i,j]} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Сумма матрицы 1 и матрицы 2:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr1[i,j] + arr2[i,j]} ");
+                }
+                Console.WriteLine("");
+            }
+
+            int result_composition = 0;
+
+            for (int k = 0; k < ROWS1; k++)
+            {
+                for (int l = 0; l < COLS1; l++)
+                {
+                    for (int j = 0; j < COLS1; j++)
+                    {
+                        result_composition = result_composition + arr1[k,j] * arr2[j,l];
+                    }
+                    arr_composition[k,l] = result_composition;
+                    result_composition = 0;
+                }
+            }
+            
+            Console.WriteLine("Перемножение матрицы 1 и матрицы 2:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr_composition[i,j]} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.Write("Введите число: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Перемножение матрицы 1 и числа:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr1[i, j]*a} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Перемножение матрицы 2 и числа:");
+
+            for (int i = 0; i < ROWS1; i++)
+            {
+                for (int j = 0; j < COLS1; j++)
+                {
+                    Console.Write($"{arr2[i, j] * a} ");
+                }
+                Console.WriteLine("");
+            }
+
+#endif
+            // Задание 5
+
+#if false
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            string str1, str2;
+
+            if (str.IndexOf("+") > 0)
+            {
+                str1 = str.Substring(0, str.Length - (str.IndexOf("+") + 1));
+                str2 = str.Substring(str.IndexOf("+") + 1);
+                Console.WriteLine($"Сумма {Convert.ToInt32(str1)} и {Convert.ToInt32(str2)} равна: {Convert.ToInt32(str1) + Convert.ToInt32(str2)}");
+            }
+            else if (str.IndexOf("-") > 0)
+            {
+                str1 = str.Substring(0, str.Length - (str.IndexOf("-") + 1));
+                str2 = str.Substring(str.IndexOf("-") + 1);
+                Console.WriteLine($"Разность {Convert.ToInt32(str1)} и {Convert.ToInt32(str2)} равна: {Convert.ToInt32(str1) - Convert.ToInt32(str2)}");
+            }
+            else Console.WriteLine("Введено НЕ арифметическое выражение");    
+#endif
+            // Задание 6
+
+#if false
+            //  today is a good day for walking. i will try to walk near the sea.
+            //  today is a good day for walking.i will try to walk near the sea.
+            //  today is a good day for walking! i will try to walk near the sea.
+            //  today is a good day for walking!i will try to walk near the sea.
+            //  today is a good day for walking? i will try to walk near the sea.
+            //  today is a good day for walking?i will try to walk near the sea.
+
+            Console.WriteLine("Введите текст:");
+            string str = Console.ReadLine().ToLower();
+            char[] str1 = new char[str.Length];
+
+            str.ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == 0 && str[i] != ' ') str1[i] = char.ToUpper(str[i]);
+                else if (i == 0 && str[i + 1] != ' ')
+                {
+                    str1[i + 1] = char.ToUpper(str[i + 1]);
+                    str1[i] = ' ';
+                    i++;
+                }
+                else if ((str[i] == '.' || str[i] == '!' || str[i] == '?') && i + 1 < str.Length && str[i + 1] == ' ' && i + 2 < str.Length)
+                {
+                    str1[i] = str[i];
+                    str1[i + 1] = str[i + 1];
+                    str1[i + 2] = char.ToUpper(str[i + 2]);
+                    i += 2;
+                }
+                else if ((str[i] == '.' || str[i] == '!' || str[i] == '?') && i + 1 < str.Length && str[i + 1] != ' ')
+                {
+                    str1[i] = str[i];
+                    str1[i + 1] = char.ToUpper(str[i + 1]);
+                    i++;
+                }
+                else str1[i] = str[i];
+            }
+
+            foreach (char i in str1)
+                Console.Write($"{i}"); 
+#endif
+            // Задание 7
+
+#if false
+            // To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer. The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles,
+
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Введите плохое слово:");
+            string badWord = Console.ReadLine();
+            string correctText = text.Replace(badWord, "***");  // проверка как есть, например, если введено слово в верхнем регистре
+            badWord = badWord.ToLower();
+            correctText = correctText.Replace(badWord, "***");  // проверка на слово в нижнем регистре
+            badWord = badWord.Substring(0, 1).ToUpper() + (badWord.Length > 1 ? badWord.Substring(1, badWord.Length - 1) : "");
+            correctText = correctText.Replace(badWord, "***");  // проверка, если первая буква заглавная
+
+            Console.Write(correctText); 
+#endif
 
             Console.ReadLine();
         }
